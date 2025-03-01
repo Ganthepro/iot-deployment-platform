992ead53f7bac6e14626adc2db75751ee4ba4d70
Ganthepro <gan_zaza_12@hotmail.com>
2025-03-01 23:51:56 +0700

    feat: add Dockerfile and docker-compose for containerized app setup, configure Vite preview settings

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,10 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
+    preview: {
+        port: 8080,
+        strictPort: true,
+    },
     resolve: {
         alias: {
             "@": path.resolve(__dirname, "./src"),
